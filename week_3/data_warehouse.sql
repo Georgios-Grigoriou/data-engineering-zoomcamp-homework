49f3d714a09b2f49c4489e9060206dd37d4ab873
Georgios Grigoriou <georgios.grigoriou@burohappold.com>
2023-02-13 20:16:47 +0000

    Update data_warehouse.sql

--- week_3/data_warehouse.sql
+++ week_3/data_warehouse.sql
@@ -17,14 +17,13 @@ Question 1
 
 -- Count of fhv trips
 SELECT count(*)
-FROM `braided-case-375422.dezoomcamp.fhv_trip_data`
+FROM `<project id>.dezoomcamp.fhv_trip_data`
 WHERE DATE(pickup_datetime) BETWEEN "2019-01-01" AND "2019-12-31";
 
 Or
 
 -- Count of fhv trips
-SELECT count(*) FROM `braided-case-375422.dezoomcamp.fhv_nonpartitioned_trip_data`;
-We get the result of 43,244,696
+SELECT count(*) FROM `<project id>.dezoomcamp.fhv_nonpartitioned_trip_data`;
 
 Question 2
 
@@ -35,21 +34,21 @@ SELECT COUNT(DISTINCT(Affiliated_base_number)) FROM `<project id>.dezoomcamp.fhv
 
 Question 3 
 
-SELECT count(1) FROM `braided-case-375422.dezoomcamp.fhv_nonpartitioned_trip_data`
+SELECT count(1) FROM `<project id>.dezoomcamp.fhv_nonpartitioned_trip_data`
 WHERE PUlocationID is NULL and DOlocationID is NULL;
 
 Question 5 SQL
 
 -- Create partition table
-CREATE OR REPLACE TABLE `braided-case-375422.dezoomcamp.fhv_partitioned_trip_data_2019`
+CREATE OR REPLACE TABLE `<project id>.dezoomcamp.fhv_partitioned_trip_data_2019`
 PARTITION BY DATE(pickup_datetime)
 CLUSTER BY Affiliated_base_number
 AS SELECT * FROM `braided-case-375422.dezoomcamp.fhv_trip_data`;
 
 -- WITH partitioning on pickup_datetime and clustering on Affiliated_base_number
-SELECT DISTINCT Affiliated_base_number,  FROM `braided-case-375422.dezoomcamp.fhv_nonpartitioned_trip_data`
+SELECT DISTINCT Affiliated_base_number,  FROM `<project id>.dezoomcamp.fhv_nonpartitioned_trip_data`
 WHERE DATE(pickup_datetime) BETWEEN '2019-03-01' AND '2019-03-31';
 
 -- WITH partitioning on pickup_datetime and clustering on Affiliated_base_number
-SELECT DISTINCT Affiliated_base_number,  FROM `braided-case-375422.dezoomcamp.fhv_nonpartitioned_trip_data`
+SELECT DISTINCT Affiliated_base_number,  FROM `<project id>.dezoomcamp.fhv_nonpartitioned_trip_data`
 WHERE DATE(pickup_datetime) BETWEEN '2019-03-01' AND '2019-03-31';
